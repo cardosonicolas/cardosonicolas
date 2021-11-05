51c81691ed71f7f183f84a5c0b541f9333e2ef6a
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2021-11-05 11:42:46 -0300

    add simple responsive navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,73 @@
+import * as React from "react";
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+const Links = ["About", "Contact", "Portfolio", "Skills"];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("blue.50", "blue.900")} p={"1em"}>
+        <Flex alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            bg={"inherit"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <Box>{"<Nicolás />"}</Box>
+          <HStack spacing={8} alignItems={"center"}>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
